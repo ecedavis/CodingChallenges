6fff3bed50d739a87c6af4b5b035e8a2db70c1aa
Mark Davis <13800653+ecedavis@users.noreply.github.com>
2018-08-10 22:31:06 -0500

    Create day01.py

---
+++ day01.py
@@ -0,0 +1,9 @@
+import sys
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+sys.stdout.write(input_string)
