a3945dd5092eca1f75cda897a35c687ce673b415
Mark Davis <13800653+ecedavis@users.noreply.github.com>
2018-08-10 23:45:53 -0500

    Create missinginteger.java

---
+++ missinginteger.java
@@ -0,0 +1,55 @@
+public class Solution {
+    public static void main(String args[])
+    {
+        int[] arr = {1, 3, 6, 4, 1, 2};
+        //System.out.println("length = " + arr.length);
+        
+        //System.out.println("output = " + solution(arr));
+    }
+    public static int solution(int[] A) {
+        // write your code in Java SE 8
+        int N = A.length;
+        int max = N+1;
+        int[] keeps = new int[N];
+        
+        
+        //trivial solution 
+        int min = 1;
+        
+        //nontrivial solution
+        if (N> 0)
+        {
+            int at;
+            //iterate through N integers
+            for (int i=0; i<N; i++)
+            {
+                at = A[i];
+                
+                //System.out.println("at=" + at + ", min=" + min + ", max=" + max);
+                //if within bounds, add to array
+                if(at<min || at>=max)
+                {
+                    max--;
+                    continue;
+                }
+                else 
+                {
+                    keeps[at-1] = at;
+                }
+                
+            }
+            
+            for (int i=0; i<N; i++)
+            {
+                //System.out.println("keeps=" + keeps[i] + ", i=" + i);
+                if (keeps[i]!=i+1)
+                {
+                    return i+1;
+                }
+            }
+            return max;
+        }
+        return min;
+        
+    }
+}
